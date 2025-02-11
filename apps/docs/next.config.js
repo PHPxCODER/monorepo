ebcfcf466e79d5be41d82a596d24def517fe7780
Subhadeep <codevizag@gmail.com>
2025-02-11 20:27:53 +0530

    test

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: "/docs", // Serve docs under /docs
+  assetPrefix: "/docs" // Ensures assets load correctly
+};
 
 export default nextConfig;
